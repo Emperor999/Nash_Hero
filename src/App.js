05b3ff14a1d5cc7a1541135218d01cfcec511822
Emperor999 <minhnguyenngoc2809@gmail.com>
2021-11-02 15:44:44 +0700

    HeroNew

--- src/App.js
+++ src/App.js
@@ -1,68 +1,87 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import React from 'react';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loader from 'react-loader-spinner';
+import HeroTag from './Component/HeroTag/HeroTag';
+import HeroDetail from './Component/HeroDetail/HeroDetail';
+import HeroMessage from './Component/HeroMessage/HeroMessage';
+import React from 'react';
 
-export default function App() {
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
   const [heroes, setHeroes] = useState([]);
-  const [id, setId] = useState('');
-  const [heroName, setHeroName] = useState('');
-  const [message, setMessage] = useState('');
-  const [details, setdetaiils] = useState('');
+  const [selecting, setSelecting] = useState({});
+  const [message, setMessage] = useState([]);
+  const [toggleMessage, setToggleMessage] = useState(true);
+
+  const handleName = (event) => {
+    let newHeroes = [...heroes];
+    newHeroes.find((hero) => hero.id === selecting.id).name =
+      event.target.value;
+    setHeroes(newHeroes);
+  };
+
+  const clearMessage = () => {
+    setToggleMessage(false);
+    setMessage([]);
+  };
+  const handleSelecting = (hero) => {
+    setMessage((m) => m.concat([`HeroComponent: Selected hero id=${hero.id}`]));
+    setToggleMessage(true);
+    setSelecting(hero);
+  };
   useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const res = await axios.get(
-          'https://60dff0ba6b689e001788c858.mockapi.io/heroes'
-        );
-        const data = res.data;
-        console.log(data);
-        setHeroes(data);
-      } catch (err) {
-        console.log(err);
-      }
+    let didCancel = false;
+    axios({
+      method: 'GET',
+      url: 'https://60dff0ba6b689e001788c858.mockapi.io/heroes',
+    })
+      .then((response) => {
+        if (!didCancel) {
+          setIsLoading(false);
+          setHeroes(response.data);
+          setMessage((m) => m.concat(['HeroService: fetched heroes']));
+        }
+      })
+      .catch((error) => {
+        if (!didCancel) {
+          setIsLoading(false);
+          setErrorMessage(error.message);
+        }
+      });
+
+    return () => {
+      didCancel = true;
     };
-    fetchPost();
   }, []);
   return (
     <div className="App">
-      {heroes.map((hero, index) => (
-        <div>
-          <ul className="heroes">
-            <li
-              className="li"
-              key={index}
-              onClick={() => {
-                setdetaiils(' details');
-                setId(hero.id);
-                setHeroName(hero.name);
-                setMessage(
-                  (previousMes) =>
-                    previousMes +
-                    `HeroesComponent: Selected hero id=${hero.id}\n`
-                );
-              }}
-            >
-              <span className="badge">{hero.id}</span> {hero.name}
-            </li>
-          </ul>
-        </div>
-      ))}
-      <br />
-      <h3>
-        {heroName.toUpperCase()}
-        {details}
-      </h3>
-      <div>id : {id}</div>
-      Hero Name : <input type="text" defaultValue={heroName} />
-      <br />
-      <button onClick={() => setMessage('')}>Clear message</button>
-      <h1 style={{ color: 'red' }}>Message</h1>
-      <h2>
-        {message.split('\n').map((mes, index) => (
-          <p key={index}>{mes}</p>
+      <h1 className="title">Tour of Heroes</h1>
+      <p className="sub-title">My Heroes</p>
+      {isLoading && (
+        <Loader type="RevolvingDot" color="#2BAD60" height={100} width={100} />
+      )}
+      {errorMessage && <p>{errorMessage}</p>}
+      <div className="hero-list">
+        {heroes.map((hero) => (
+          <HeroTag
+            key={hero.id}
+            hero={hero}
+            handleSelecting={handleSelecting}
+            selecting={selecting}
+          />
         ))}
-      </h2>
+      </div>
+      {Object.keys(selecting).length !== 0 && (
+        <HeroDetail hero={selecting} handleName={handleName}></HeroDetail>
+      )}
+
+      {toggleMessage && (
+        <HeroMessage message={message} clear={clearMessage}></HeroMessage>
+      )}
     </div>
   );
 }
+
+export default App;
